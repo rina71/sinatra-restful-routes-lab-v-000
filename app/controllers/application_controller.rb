059df12a42491e2fc60bfef2957daadd8863fc30
Yurina Shin <yurinas@gmail.com>
2018-10-02 16:44:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    @recipe = Recipe.new(:name=>params[:name],:ingredients=>params[:ingredients], :cook_time=>params[:cook_time])
-    @recipe.save
-
-    redirect to '/recipes/#{@recipe.id}'
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes' do
